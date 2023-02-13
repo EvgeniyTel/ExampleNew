1c5104ada3100ac295dd26769a89953dbf53783d
EvgeniyTel <zheniatelegin1995@yandex.ru>
2023-02-13 17:29:22 +0300

    Examp11_2

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,4 +1,5 @@
-﻿void FillArray(int[] collection)
+﻿Console.Clear();
+void FillArray(int[] collection)
 {
   int lenght = collection.Length;
   int index = 0;
@@ -17,13 +18,37 @@ void PrintArray(int[] col)      //void-метод не возвращает
   int position = 0;
   while (position < count)
   {
-        Console.WriteLine(col[position]);
-        position++;
+    Console.WriteLine(col[position]);
+    position++;
   }
 
+}
+//поиск позиции с помощью метода
+int Indexof(int[] collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int position = -1;//если элемента нет
+  while (index < count)
+  {
+    if (collection[index] == find)
+    {
+      position = index;
+      break;
+    }
+    index++;
+  }
+  return position;
 }
 
 int[] array = new int[10];
 
 FillArray(array);
+array[4] = 8;
+array[6] = 8;
 PrintArray(array);
+
+Console.WriteLine();
+
+int pos = Indexof(array, 43);
+Console.WriteLine(pos);
