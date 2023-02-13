0c706b5b602db01fa48959f38000c836eecc59c2
EvgeniyTel <zheniatelegin1995@yandex.ru>
2023-02-13 17:04:02 +0300

    Examp10

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 2, 3, 6, 56, 6, 8, 67 };
+
+int n = array.Length;
+int find = 6;
+
+int index = 0;
+
+while (index < n)
+{
+
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+
+    index++;
+}
